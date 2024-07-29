75666e50eab2339a5a0ba8d11f48df87ebdfdbb5
talentedbug <talentedbug@tutamail.com>
2024-07-29 16:33:02 +0800

    First-step build, bugs exist

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# mailbin
-A mailing list-style blog framework for static pages
+```
+MAILBIN - A mailing list-style blog framework for static pages
+
+MailBin is a blog framework that looks like a mailing list, aimed to bring 
+your readers ease of reading, compatibility and light weight.
+
+Main features:
+
+- Minimal CSS: We've got no CSS files and the only lines are for displaying 
+  monospace fonts.
+- Pure HTML: Simple code leads to few requirements for browsers. Even Firefox 
+  2 can load it correctly!
+- Portability: Written in Bash script, MailBin can be used without pain on any 
+  POSIX-compatible system.
+- Traditional style: Lines are limited to 80 words per line strictly, which 
+  *nix users are crazy about.
+
+MailBin is meant to imitate mailing list, which is considered outdated by 
+many. However, just like RSS, it is attractive for its feeling of simplicity. 
+For an example you can read https://lkml.org/, the Linux Kernel Mailing List 
+Archive.
+
+MailBin use *plain* text as source files. It means no images, no extra files 
+and no fancy text effects are allowed. If you really need to display an image 
+or so, please place it in the `asset` directory and cite it as 
+https://yourdomain/asset/xxx.xxx.
+
+To install it on your system, simply clone this repository. Modify the 
+settings according to your preference, which is not an optional step. Then run 
+`bash mkmail.bash` to generate all the things.
+
+For guides to configuration, please directly read comments in the code. It's 
+detailed enough that you can customize anything available. Due to my laziness 
+I created no Wiki pages for it, so please use the code instead.
+
+If you'd like to deploy MailBin on a static page service, please upload the 
+directory or choose it from a Git provider. Then set as following:
+
+- "Build command" and so: "bash mkmail.bash"
+- "Site root directory" and so: "site"
+
+MailBin is licensed under the Unlicense. You are free to do anything to these 
+code.
+```
---
+++ mkmail.bash
@@ -0,0 +1,122 @@
+# MailBin generation program
+
+# Check the existence of configuration file
+if [[ ! -f ./mailbin.conf ]];
+then
+    echo "[ERROR] No configuration found: mailbin.conf"
+    exit 1
+fi
+
+# Check the definition of important variables
+source ./mailbin.conf
+if [[ -z "${SITE_NAME+x}" || -z "${FROM_NAME+x}" || -z "${FROM_EMAIL+x}" || -z "${TIME_ZONE+x}" || -z "${LINE_LENGTH+x}" ]];
+then
+    echo "[ERROR] Some important variables not set: mailbin.conf"
+    exit 1
+fi
+
+# Check the existence of at least one source file
+if [[ ! -d "./src" ]];
+then
+    echo "[ERROR] Source directory not found: src"
+    exit 1
+fi
+if [[ -z "$(ls -A ./src 2>/dev/null)" ]];
+then
+    echo "[ERROR] No source files was found in source directory: src"
+    exit 1
+fi
+
+echo "[INFO] Basic checks passed. No error reported."
+
+# Start dealing with texts
+# Currently no CJK-support is implemented due to their complexity
+
+# Clean old files
+rm -rf ./site 2>/dev/null
+
+# Create basic structure
+mkdir ./site
+touch ./site/index.html
+mkdir ./site/asset
+mkdir ./site/mail
+
+# Write index.html
+printf "<!DOCTYPE html>\n<head>\n    <title>${SITE_NAME}</title>\n<head>\n" >> ./site/index.html
+printf "<body>\n" >> ./site/index.html
+printf "    <h1>${SITE_NAME}</h1>\n" >> ./site/index.html
+printf "    <h2>About</h2>\n" >> ./site/index.html
+printf "    <pre>\n" >> ./site/index.html
+fold -w $((LINE_LENGTH)) -s ./src/about.txt >> ./site/index.html
+printf "    </pre>\n" >> ./site/ind ex.html
+printf "    <h2>Index</h2>\n" >> ./site/index.html
+for doc in $(ls -t ./src);
+do
+    bn=$(basename "${doc}")
+    if [[ "${bn}" = "about.txt" ]];
+    then
+        continue
+    fi
+    printf "    <h3><a href=\"./mail/${bn%.*}.html\">${bn%.*}</a></h3>\n" >> ./site/index.html
+    printf "    <p style=\"font-family: monospace;\">\n" >> ./site/index.html
+    printf "        <b>Subject</b> &nbsp;${doc%.*}<br>\n" >> ./site/index.html
+    printf "        <b>From &nbsp;&nbsp;</b> &nbsp;${FROM_NAME} &lt;${FROM_EMAIL}&gt;<br>\n" >> ./site/index.html
+    printf "        <b>Date &nbsp;&nbsp;</b> &nbsp;$(TZ=${TIME_ZONE} date -r "./src/${bn}" +"%Y-%m-%d %H:%M %Z")<br>\n" >> ./site/index.html
+    printf "        <br>\n" >> ./site/index.html
+    printf "    </p>\n" >> ./site/index.html
+done
+printf "</body>\n" >> ./site/index.html
+
+echo "[INFO] index.html created."
+
+# Write pages
+cnt=0
+for doc in ./src/*;
+do
+    if [[ $doc = "about.txt" ]];
+    then
+        continue
+    fi
+    cnt=$((cnt + 1))
+    if [[ $cnt = 1 ]];
+    then
+        s=$(basename "${doc}")
+        lp=${s%.*}
+        continue
+    fi
+    echo $lp
+    s=$(basename "${doc}")
+    bn=${s%.*}
+    if [[ $cnt = 1 ]];
+    then
+        llp="${lp}"
+    fi
+    touch "./site/mail/${lp}.html"
+    printf "<!DOCTYPE html>\n<head>\n    <title>${SITE_NAME} - ${lp}</title>\n<head>\n" >> "./site/mail/${lp}.html"
+    printf "<body>\n" >> "./site/mail/${lp}.html"
+    printf "    <p><a href="./${llp}.html">[prev]</a> <a href="./${bn}.html">[next]</a></p>\n" >> "./site/mail/${lp}.html"
+    printf "    <p style=\"font-family: monospace;\">\n" >> "./site/mail/${lp}.html"
+    printf "        <b>Subject</b> &nbsp;${doc%.*}<br>\n" >> "./site/mail/${lp}.html"
+    printf "        <b>From &nbsp;&nbsp;</b> &nbsp;${FROM_NAME} &lt;${FROM_EMAIL}&gt;<br>\n" >> "./site/mail/${lp}.html"
+    printf "        <b>Date &nbsp;&nbsp;</b> &nbsp;$(TZ=${TIME_ZONE} date -r "./src/${bn}" +"%Y-%m-%d %H:%M %Z")<br>\n" >> "./site/mail/${lp}.html"
+    printf "        <br>\n" >> ./site/index.html
+    printf "    </p>\n" >> ./site/index.html
+    printf "    <pre style=\"font-family: monospace\">\n" >> "./site/mail/${lp}.html"
+    fold -w $((LINE_LENGTH)) -s >> "./site/mail/${lp}.html"
+    printf "    </pre>\n" >> "./site/mail/${lp}.html"
+    llp="${lp}"
+    lp="${bn}"
+done
+touch "./site/mail/${lp}.html"
+printf "<!DOCTYPE html>\n<head>\n    <title>${SITE_NAME} - ${lp}</title>\n<head>\n" >> "./site/mail/${lp}.html"
+printf "<body>\n" >> "./site/mail/${lp}.html"
+printf "    <p style=\"font-family: monospace;\">\n" >> "./site/mail/${lp}.html"
+printf "        <b>Subject</b> &nbsp;${doc%.*}<br>\n" >> "./site/mail/${lp}.html"
+printf "        <b>From &nbsp;&nbsp;</b> &nbsp;${FROM_NAME} &lt;${FROM_EMAIL}&gt;<br>\n" >> "./site/mail/${lp}.html"
+printf "        <b>Date &nbsp;&nbsp;</b> &nbsp;$(TZ=${TIME_ZONE} date -r "./src/${bn}" +"%Y-%m-%d %H:%M %Z")<br>\n" >> "./site/mail/${lp}.html"
+printf "        <br>\n" >> ./site/index.html
+printf "    </p>\n" >> ./site/index.html
+printf "    <p><a href="./${llp}.html">[prev]</a> <a href="./${bn}.html">[next]</a></p>\n" >> "./site/mail/${lp}.html"
+printf "    <pre style=\"font-family: monospace\">\n" >> "./site/mail/${lp}.html"
+fold -w $((LINE_LENGTH)) -s >> "./site/mail/${lp}.html"
+printf "    </pre>\n" >> "./site/mail/${lp}.html"
